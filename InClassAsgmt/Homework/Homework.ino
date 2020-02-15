debb502cd4534854b739db26cd2974089580f3f9
Manatsafang <59952825+Manatsafang@users.noreply.github.com>
2020-02-15 13:08:57 +0700

    Update Homework.ino

--- InClassAsgmt/Homework/Homework.ino
+++ InClassAsgmt/Homework/Homework.ino
@@ -1,79 +1,52 @@
-int sensor_l = D2;
-int LED_R = D4;
-int sensor_r = D3;
-const int trigPin = D11;
-const int echoPin = D10;
-long duration;
-int distance;
-
-
+#include <HCSR04.h>
+HCSR04 hc(D11,D10); //trig,echo
+int SensorL=D2; //right LED
+int SensorR=D3; //left LED
+int LEDRight=D4; //Yellow LED
 void setup() {
-  Serial.begin(9600);
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(sensor_l, INPUT);
-  digitalWrite(LED_BUILTIN, 0);
-  digitalWrite(sensor_l, 0); //right
+   Serial.begin(38400);
+   pinMode(LED_BUILTIN, OUTPUT); //left light
+   pinMode(LEDRight, OUTPUT); //right light
+   pinMode(SensorL,INPUT);  
+   pinMode(SensorR,INPUT); 
 
-  pinMode(LED_R, OUTPUT);
-  pinMode(sensor_r, INPUT);
-  digitalWrite(LED_R, 0);
-  digitalWrite(sensor_r, 0);//left
-
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT); //ultrasonic
 }
 
 void loop() {
-  int a = digitalRead(sensor_l);
-  int b = digitalRead(sensor_r);
+ 
 
-  Serial.println(b);
-  if (a == 0) {
+ if(digitalRead(SensorL)==LOW) //detectLeft
+ {
+  digitalWrite(LED_BUILTIN, LOW);
+  digitalWrite(LEDRight, HIGH);
+   delay(100);
+ }
+  else if(digitalRead(SensorR)==LOW)//detectRight
+  {
     digitalWrite(LED_BUILTIN, HIGH);
-    if (b == 0) {
-      digitalWrite(LED_R, HIGH);
+    digitalWrite(LEDRight, LOW);
+     delay(100);
+  }
+    else if(hc.dist()<20)//detectFront20cm
+    { digitalWrite(LED_BUILTIN,LOW);
+    digitalWrite(LEDRight,LOW);
+    delay(100);
     }
-    else if (b == 1) {
-      digitalWrite(LED_R, LOW);
+    else if(hc.dist()>40)//ObjFar40cm
+    { digitalWrite(LED_BUILTIN,HIGH);
+    digitalWrite(LEDRight,HIGH);
+    delay(100);
     }
-  }
-  else if (a == 1) {
-    digitalWrite(LED_BUILTIN, LOW);
-    if (b == 0) {
-      digitalWrite(LED_R, HIGH);
+    else if(hc.dist()>=20 && hc.dist()<=40)//detectFront20to40cm
+    { 
+    
+      digitalWrite(LED_BUILTIN,HIGH);
+       digitalWrite(LEDRight,HIGH);
+       delay(1000);
     }
-    else if (b == 1) {
-      digitalWrite(LED_R, LOW);
-    }
+   else
+   {digitalWrite(LED_BUILTIN,LOW);
+       digitalWrite(LEDRight,LOW);
+   }
+   Serial.println(hc.dist());
   }
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2); // Sets the trigPin on HIGH state for 10 micro seconds
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  // Reads the echoPin, returns the sound wave travel time in microseconds
-  duration = pulseIn(echoPin, HIGH);
-  // Calculating the distance
-  distance = (duration * 0.034 / 2) ;
-  // Prints the distance on the Serial Monitor
-  Serial.print("Distance: ");
-  Serial.println(distance);
-  // put your main code here, to run repeatedly:
-  if (distance < 20) {
-    digitalWrite(LED_BUILTIN, LOW);
-    digitalWrite(LED_R, LOW);
-  }
-  else if (distance > 40)
-  { digitalWrite(LED_BUILTIN, HIGH);
-    digitalWrite(LED_R, HIGH);
-  }
-  else if (distance >= 20 && distance <= 40) {
-    digitalWrite(LED_BUILTIN, HIGH);
-    digitalWrite(LED_R, HIGH);
-    delay(500);
-    digitalWrite(LED_BUILTIN, LOW);
-    digitalWrite(LED_R, LOW);
-    delay(500);
-  }
-delay(100);
-}
